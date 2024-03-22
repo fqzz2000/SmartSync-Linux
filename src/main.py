e650ceae3137ab9e9fa4746710eeb4f58a9667e2
tenki <tq22@duke.edu>
2024-03-22 17:37:47 -0400

    chore: remove secret info

--- src/main.py
+++ src/main.py
@@ -6,18 +6,15 @@ from model import DropBoxModel
 import logging
 if __name__ == "__main__":
     import sys
-    # if len(sys.argv) != 2:
-    #     print("Usage: {} <token>".format(sys.argv[0]))
-    #     sys.exit(1)
-    # TOKEN = sys.argv[1]
-    # db = DropboxInterface(TOKEN)
-    db = DropboxInterface("sl.Bx4k8Jo7wYwZiG3fUB9io_SBs6LhReSbtZIa7jI6TW7kCP41dV4XABqpBghyFRO1g1UBnBa4p7xVgLauuiyxM9HUkIzHS9NSHzMRS88L69ZDhzuE2ddEpVP2or0WtC2FNbSHJWfQryQP64ef2P-04AU")
+    if len(sys.argv) != 2:
+        print("Usage: {} <token>".format(sys.argv[0]))
+        sys.exit(1)
+    TOKEN = sys.argv[1]
+    db = DropboxInterface(TOKEN)
     rootdir = "/home/tq22/ece566/SmartSync-Linux/cache"
     model = DropBoxModel(db)
     model.downloadAll(rootdir)
     logging.basicConfig(level=logging.DEBUG)
     # fuse = FUSE(FuseDropBox(rootdir, model), "/home/tq22/ece566/SmartSync-Linux/dropbox", foreground=True, allow_other=True)
-
-    # sl.Bx4k8Jo7wYwZiG3fUB9io_SBs6LhReSbtZIa7jI6TW7kCP41dV4XABqpBghyFRO1g1UBnBa4p7xVgLauuiyxM9HUkIzHS9NSHzMRS88L69ZDhzuE2ddEpVP2or0WtC2FNbSHJWfQryQP64ef2P-04AU
 
     
