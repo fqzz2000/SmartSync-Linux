da3100a853c8947d8971089d6a17654e24440a5e
Tianji Qiang <104676212+Tenki99@users.noreply.github.com>
2024-04-02 21:50:36 -0400

    21 oauth webhook subscription (#23)
    * feat: add login
    
    * debug: debug for login
    
    * feat: finish webhook
    
    * chore: modify the url of webhook server
    
    * chore: small change
    
    * feat: run login server into a thread of main
    
    * feat: run login server with multi process
    
    * chore: some modification
    
    * debug: close the flask reloader
    
    * feat: add heartbeat
    
    * chore: change pem file name
    
    * chore: client ignore heartbeat info
    
    * chore: add config file, delete redundant file
    
    * debug: start subscribe thread after initializing the model
    
    ---------
    
    Co-authored-by: tenki <tq22@duke.edu>

---
+++ src/config.py
@@ -0,0 +1,4 @@
+APP_KEY = "p379vmpas0tf58c"
+SUBSCRIBE_URL = "https://vcm-39026.vm.duke.edu:5002/events"
+TMP_DIR = "/tmp/dropbox"
+REDIRECT_URI = "http://localhost:5000/oauth2/callback"
--- src/main.py
+++ src/main.py
@@ -3,95 +3,131 @@ from data import DropboxInterface
 from fuselayer import FuseDropBox
 from lib import FUSE
 from model import DropBoxModel
-import logging
 import atexit
-import dropbox
-from dropbox import DropboxOAuth2FlowNoRedirect
-import datetime
-import time
 import os
-import signal
 import sys
 import argparse
 import subprocess
 import shutil
-import webbrowser
 import daemon
 from daemon import pidfile
+import webbrowser
+import threading
+import requests
+from loguru import logger
+from multiprocessing import Process, Queue
+from flask import Flask, request, redirect, session
+import dropbox
+import logging
+import config
 
-APP_KEY = "p379vmpas0tf58c"
 WORKING_DIR = os.path.expanduser("~/Desktop")
-TMP_DIR = "/tmp/dropbox"
-pid_file = os.path.join(TMP_DIR, "dropbox.pid")
+pid_file = os.path.join(config.TMP_DIR, "dropbox.pid")
+auth_token = None
+user_id = None
+login_app = Flask(__name__)
+login_app.secret_key = os.urandom(24)
+logging.getLogger('werkzeug').setLevel(logging.CRITICAL)
+queue = Queue()
 
+auth_flow = dropbox.DropboxOAuth2Flow(config.APP_KEY, config.REDIRECT_URI, session, 'dropbox-auth-csrf-token', use_pkce=True, token_access_type='offline')
+
+@login_app.route('/start')
+def start():
+    authorize_url = auth_flow.start()
+    return redirect(authorize_url)
+
+@login_app.route('/oauth2/callback')
+def callback():
+    try:
+        oauth_result = auth_flow.finish(request.args)
+        queue.put(oauth_result.access_token)
+        return 'Success'
+    except Exception as e:
+        return f"Error: {e}"
+    
 # def signal_handler(sig, frame):
 #     print("Caught signal", sig)
 #     model.stop()
 #     sys.exit(0)
 # signal.signal(signal.SIGTERM, signal_handler)
 
-def main():
-    parser = argparse.ArgumentParser(description="Dropbox CLI")
-    subparsers = parser.add_subparsers(dest='command')
+def run_login_server():
+    login_app.run(debug=False, port=5000, use_reloader=False)
 
-    parser_start = subparsers.add_parser('start', help='Start the dropbox daemon')
-    parser_start.set_defaults(func=start_daemon)
+def listen_for_events(url):
+    while True:
+        try:
+            response = requests.get(url, stream=True)
+            for line in response.iter_lines():
+                if line and line[0] != b':'[0]:
+                    # log the line with logging
+                    logger.warning(f'Event: {line}')
 
-    parser_stop = subparsers.add_parser('stop', help='Stop the dropbox daemon')
-    parser_stop.set_defaults(func=stop_daemon)
-
-    args = parser.parse_args()
-
-    if args.command is None:
-        parser.print_help()
-    else:
-        args.func()
-
+        except Exception as e:
+            print(f"Error listening for events: {e}")
+    
 def start_daemon():
-    if not os.path.exists(TMP_DIR):
-        os.mkdir(TMP_DIR)
+    # create directories and clearing previous logs
+    if not os.path.exists(config.TMP_DIR):
+        os.mkdir(config.TMP_DIR)
     if not os.path.exists(WORKING_DIR):
         os.mkdir(WORKING_DIR)
     if not os.path.exists(os.path.join(WORKING_DIR, ".cache")):
         os.mkdir(os.path.join(WORKING_DIR, ".cache"))
     if not os.path.exists(os.path.join(WORKING_DIR, "dropbox")):
         os.mkdir(os.path.join(WORKING_DIR, "dropbox"))
-
-    auth_flow = DropboxOAuth2FlowNoRedirect(APP_KEY, use_pkce=True, token_access_type='offline')
-
-    authorize_url = auth_flow.start()
-    webbrowser.open(authorize_url)
-    print("1. Click \"Allow\" (you might have to log in first).")
-    print("2. Copy the authorization code.")
-    print("(Visit this url if your web browser fails to boot:" + authorize_url + ")")
-    auth_code = input("Enter the authorization code here: ").strip()
-
-    try:
-        oauth_result = auth_flow.finish(auth_code)
-    except Exception as e:
-        print('Error: %s' % (e,))
-        exit(1)
-    token = oauth_result.access_token
-    print("Start setting up your dropbox...")
-    db = DropboxInterface(token)
     rootdir = os.path.join(WORKING_DIR, ".cache")
-    if os.path.exists(os.path.join(TMP_DIR, "dropbox.log")):
-        os.unlink(os.path.join(TMP_DIR, "dropbox.log")) 
-    if os.path.exists(os.path.join(TMP_DIR, "std_out.log")):
-        os.unlink(os.path.join(TMP_DIR, "std_out.log")) 
-    if os.path.exists(os.path.join(TMP_DIR, "std_err.log")):
-        os.unlink(os.path.join(TMP_DIR, "std_err.log")) 
+    if os.path.exists(os.path.join(config.TMP_DIR, "dropbox.log")):
+        os.unlink(os.path.join(config.TMP_DIR, "dropbox.log")) 
+    if os.path.exists(os.path.join(config.TMP_DIR, "std_out.log")):
+        os.unlink(os.path.join(config.TMP_DIR, "std_out.log")) 
+    if os.path.exists(os.path.join(config.TMP_DIR, "std_err.log")):
+        os.unlink(os.path.join(config.TMP_DIR, "std_err.log")) 
+
+    # fetching auth token
+    login_server_process = Process(target=run_login_server)
+    login_server_process.start()
+    authorize_url = "http://localhost:5000/start"
+    # print(f"{os.getpid()}: browser launching...")
+    webbrowser.open(authorize_url)
+    global auth_token
+    while True:
+        if not queue.empty():
+            auth_token = queue.get()
+            break;
+    print("Auth token fetched successfully!")
+    print("Terminating login flask server...")
+    login_server_process.terminate()
+    login_server_process.join()
+    
+    # setting up dropbox instance
+    os.environ['MY_APP_AUTH_TOKEN'] = auth_token
+    # print("Start setting up your dropbox...")
+    
+    # start daemon
     context = daemon.DaemonContext(
         pidfile=pidfile.TimeoutPIDLockFile(pid_file),
-        stdout=open(os.path.join(TMP_DIR, 'std_out.log'), 'w+'),
-        stderr=open(os.path.join(TMP_DIR, 'std_err.log'), 'w+'),
+        stdout=open(os.path.join(config.TMP_DIR, 'std_out.log'), 'w+'),
+        stderr=open(os.path.join(config.TMP_DIR, 'std_err.log'), 'w+'),
     )
-
     with context:
+        auth_token = os.getenv('MY_APP_AUTH_TOKEN')
+        db = DropboxInterface(auth_token)
+
+        # initialize the model
         model = DropBoxModel(db, rootdir)
         model.clearAll()
         model.downloadAll()
         atexit.register(model.clearAll)
+
+        # setting up thread listening for updates
+        global user_id
+        user_id = db.dbx.users_get_current_account().account_id
+        url = f"{config.SUBSCRIBE_URL}/{user_id}"
+        subscribe_thread = threading.Thread(target=listen_for_events, args=(url,))
+        subscribe_thread.daemon = True
+        subscribe_thread.start()
         
         try:
             fuse = FUSE(
@@ -124,5 +160,21 @@ def stop_daemon():
     
 
 if __name__ == "__main__":
-    main()
+    
+    parser = argparse.ArgumentParser(description="Dropbox CLI")
+    subparsers = parser.add_subparsers(dest='command')
+
+    parser_start = subparsers.add_parser('start', help='Start the dropbox daemon')
+    parser_start.set_defaults(func=start_daemon)
+
+    parser_stop = subparsers.add_parser('stop', help='Stop the dropbox daemon')
+    parser_stop.set_defaults(func=stop_daemon)
+
+    args = parser.parse_args()
+
+    if args.command is None:
+        parser.print_help()
+    else:
+        args.func()
+
     
