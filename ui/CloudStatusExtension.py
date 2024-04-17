7043733b48e0668a3514012a95926c661c31075e
tenki <tenki.qiang@gmail.com>
2024-04-17 09:48:06 -0400

    feat: upodate python extension

--- ui/CloudStatusExtension.py
+++ ui/CloudStatusExtension.py
@@ -9,28 +9,33 @@ import pyinotify
 class CloudStatusExtension(GObject.GObject, Nemo.InfoProvider):
     def __init__(self):
         self.local_paths = []
-        self.json_file_path = '/tmp/dropbox/metadata.json'
+        self.json_file_path = '/home/tenki/Desktop/metadata.json'
         self.target_dir_path = os.path.expanduser('~/Desktop/dropbox')
-        self.load_icon_mapping()
-        self.setup_inotify_watcher()
+        self.load_local_paths()
+#        self.setup_inotify_watcher()
     
     def load_local_paths(self):
         try:
             with open(self.json_file_path, 'r') as f:
-                self.local_paths = json.load(f).keys()
+                self.local_paths = list(json.load(f).keys())
+                print(f"load_local_path_finish: {self.local_paths}")
         except Exception as e:
-            return {}
+            self.local_paths = []
         
     def update_file_info(self, file):
         if file.get_uri_scheme() != 'file':
             return
         file_path = file.get_location().get_path()
         if file_path.startswith(self.target_dir_path):
-            file_path = file_path.replace(self.target_dir_path, '')
-            if file_path in self.local_paths:
-                file.add_emblem('emblem-default')
-            else:
-                file.add_emblem('emblem-web')
+            relative_path = os.path.relpath(file_path, self.target_dir_path)
+            self.load_local_paths()
+            print(f"currently updating: {relative_path}")
+            if relative_path != '.':
+                if relative_path in self.local_paths:
+                    file.add_emblem('emblem-default')
+                else:
+                    file.add_emblem('emblem-web')
+                print(f"emblem add finished")
 
     def setup_inotify_watcher(self):
         wm = pyinotify.WatchManager()
